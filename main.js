ae0710bb7ff45a461817b77f729b35044c7ac39a
Manahil rasheed <manahilrasheed507@gmail.com>
2024-07-21 05:01:33 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env
+// Tuples
+//syntax
+//let tuple: [type1, type2]
+// ? optional 
+//data ka structure or type pehla sha maloom hun 
+let arr = ["Manahil", 7, true, { name: "Manahil" }];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[2]);
+export {};
---
+++ main.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env
+// Tuples
+
+//syntax
+//let tuple: [type1, type2]
+
+// ? optional 
+//data ka structure or type pehla sha maloom hun 
+
+
+let arr: [string,number,boolean, {name:string}?] = ["Manahil",7,true, {name: "Manahil"}]
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[2])
